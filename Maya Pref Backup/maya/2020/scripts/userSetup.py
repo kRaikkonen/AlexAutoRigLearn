e65546c524848fbfa3696d8dfab7eeb8505d9796
kRaikkonen <42619926+kRaikkonen@users.noreply.github.com>
2023-09-28 22:04:14 -0600

    maya backup

---
+++ Maya Pref Backup/maya/2020/scripts/userSetup.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('C:\window proc script')
+
+import autorigger_with_ui
+import Create_Locators
+import Create_Joints
+import Create_Constraints
+import Twist_Loc
+import CreateIKH
+import Controllers
+
+
